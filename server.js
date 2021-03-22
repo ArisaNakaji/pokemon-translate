6d9ee08ae1e1081e5d0f67ac8e36c517b576f77b
ArisaNakaji <67836990+ArisaNakaji@users.noreply.github.com>
2021-03-22 16:24:18 +0900

    first commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static(__dirname + "/dist"));
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+  console.log("App listening on port " + port);
+});
+
+app.use("/api", (req, res) => {
+  res.send("kita-----✨✨");
+});
